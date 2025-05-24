4ae7e034d7e1bad6e9460b6405a454b5f2ef9fdc
ybbobsille <93970271+ybbobsille@users.noreply.github.com>
2025-05-24 16:16:30 -0400

    fixed error

--- src/mega/mega.py
+++ src/mega/mega.py
@@ -677,6 +677,9 @@ class Mega:
             iv = file['iv']
             meta_mac = file['meta_mac']
 
+        # Seems to happens sometime... When this occurs, files are
+        # inaccessible also in the official also in the official web app.
+        # Strangely, files can come back later.
         if 'g' not in file_data:
             raise RequestError('File not accessible anymore')
         file_url = file_data['g']
@@ -699,7 +702,7 @@ class Mega:
         with tempfile.NamedTemporaryFile(mode='w+b',
                                          prefix='megapy_',
                                          delete=False) as temp_output_file:
-        k_str = a32_to_str(k)
+            k_str = a32_to_str(k)
             counter = Counter.new(128,
                                   initial_value=((iv[0] << 32) + iv[1]) << 64)
             aes = AES.new(k_str, AES.MODE_CTR, counter=counter)
@@ -709,42 +712,38 @@ class Mega:
                                     mac_str.encode("utf8"))
             iv_str = a32_to_str([iv[0], iv[1], iv[0], iv[1]])
 
-            # Initialize tqdm progress bar
             with tqdm(total=file_size, unit='B', unit_scale=True, desc=file_name) as pbar:
                 for chunk_start, chunk_size in get_chunks(file_size):
                     chunk = input_file.read(chunk_size)
                     chunk = aes.decrypt(chunk)
                     temp_output_file.write(chunk)
-    
+
                     encryptor = AES.new(k_str, AES.MODE_CBC, iv_str)
                     for i in range(0, len(chunk) - 16, 16):
                         block = chunk[i:i + 16]
                         encryptor.encrypt(block)
-    
+
                     # fix for files under 16 bytes failing
                     if file_size > 16:
                         i += 16
                     else:
                         i = 0
-    
+
                     block = chunk[i:i + 16]
                     if len(block) % 16:
                         block += b'\0' * (16 - (len(block) % 16))
                     mac_str = mac_encryptor.encrypt(encryptor.encrypt(block))
-    
-                    # Update progress bar by the size of this chunk
+                    
                     pbar.update(len(chunk))
-    
+
                     file_info = os.stat(temp_output_file.name)
                     logger.info('%s of %s downloaded', file_info.st_size,
                                 file_size)
-
             file_mac = str_to_a32(mac_str)
             # check mac integrity
             if (file_mac[0] ^ file_mac[1],
                     file_mac[2] ^ file_mac[3]) != meta_mac:
-            raise ValueError('Mismatched mac')
-
+                raise ValueError('Mismatched mac')
             output_path = Path(dest_path + file_name)
             shutil.move(temp_output_file.name, output_path)
             return output_path
